8398f0e1b3b95f5175425a0fc4cf1688a1622858
Cato Auestad <cato@timeanddate.com>
2017-08-03 15:43:58 +0200

    Implemented BusinessDurationService

--- TimeAndDate.Services.Tests/IntegrationTests/BusinessDurationServiceTests.cs
+++ TimeAndDate.Services.Tests/IntegrationTests/BusinessDurationServiceTests.cs
@@ -1,10 +1,143 @@
 ﻿using System;
+using NUnit.Framework;
+using TimeAndDate.Services.DataTypes.Places;
+using TimeAndDate.Services.DataTypes.BusinessDays;
+
 namespace TimeAndDate.Services.Tests.IntegrationTests
 {
-    public class BusinessDurationServiceTests
+	[TestFixture()]
+	public class BusinessDurationServiceTests
     {
-        public BusinessDurationServiceTests()
-        {
-        }
-    }
+		[Test()]
+		public void Calling_BusinessDurationService_ReturnsCorrectBusinessDuration()
+		{
+			// Arrange
+			var location = new LocationId("usa/anchorage");
+			var startDate = new DateTime(2017, 12, 1);
+			var endDate = new DateTime(2018, 1, 31);
+
+			// Act
+			var svc = new BusinessDurationService(Config.AccessKey, Config.SecretKey);
+			var res = svc.GetDuration(startDate, endDate, location);
+
+			// Assert
+			Assert.AreEqual("Anchorage", res.Geography.Name);
+
+			Assert.AreEqual(61, res.Period.CalendarDays);
+			Assert.AreEqual(21, res.Period.SkippedDays);
+			Assert.AreEqual(40, res.Period.IncludedDays);
+
+			Assert.AreEqual(9, res.Period.Weekdays.SaturdayCount);
+			Assert.AreEqual(9, res.Period.Weekdays.SundayCount);
+			Assert.AreEqual(3, res.Period.Holidays.Count);
+		}
+
+		[Test()]
+		public void Calling_BusinessDurationService_WithCountryState_ReturnsCorrectBusinessDuration()
+		{
+			// Arrange
+			var startDate = new DateTime(2017, 12, 1);
+			var endDate = new DateTime(2018, 1, 31);
+
+			// Act
+			var svc = new BusinessDurationService(Config.AccessKey, Config.SecretKey);
+			var res = svc.GetDuration(startDate, endDate, "us", "us-nv");
+
+			// Assert
+			Assert.AreEqual("Nevada", res.Geography.State);
+		}
+
+		[Test()]
+		public void Calling_BusinessDurationService_WithInvalidEndDate_ThrowsException()
+		{
+			// Arrange
+			var startDate = new DateTime(2017, 12, 1);
+			var endDate = new DateTime(2017, 11, 30);
+
+			// Act
+			var svc = new BusinessDurationService(Config.AccessKey, Config.SecretKey);
+
+			// Assert
+			Assert.That(() => svc.GetDuration(startDate, endDate, "us", "us-nv"),
+				Throws.TypeOf<ArgumentException>());
+		}
+
+		[Test()]
+		public void Calling_BusinessDurationService_WithInclude_ReturnsCorrectBusinessDuration()
+		{
+			// Arrange
+			var location = new LocationId("usa/anchorage");
+			var startDate = new DateTime(2017, 12, 1);
+			var endDate = new DateTime(2018, 1, 31);
+
+			// Act
+			var svc = new BusinessDurationService(Config.AccessKey, Config.SecretKey);
+			svc.IncludeDays = true;
+
+			var res = svc.GetDuration(startDate, endDate, location);
+
+			// Assert
+			Assert.AreEqual("Anchorage", res.Geography.Name);
+
+			Assert.AreEqual(61, res.Period.CalendarDays);
+			Assert.AreEqual(40, res.Period.SkippedDays);
+			Assert.AreEqual(21, res.Period.IncludedDays);
+
+			Assert.AreEqual(9, res.Period.Weekdays.SaturdayCount);
+			Assert.AreEqual(9, res.Period.Weekdays.SundayCount);
+			Assert.AreEqual(3, res.Period.Holidays.Count);
+		}
+
+		[Test()]
+		public void Calling_BusinessDurationService_WithIncludeLastDate_ReturnsCorrectBusinessDuration()
+		{
+			// Arrange
+			var location = new LocationId("usa/anchorage");
+			var startDate = new DateTime(2017, 12, 1);
+			var endDate = new DateTime(2018, 1, 31);
+
+			// Act
+			var svc = new BusinessDurationService(Config.AccessKey, Config.SecretKey);
+			svc.IncludeLastDate = true;
+
+			var res = svc.GetDuration(startDate, endDate, location);
+
+			// Assert
+			Assert.AreEqual("Anchorage", res.Geography.Name);
+
+			Assert.AreEqual(62, res.Period.CalendarDays);
+			Assert.AreEqual(21, res.Period.SkippedDays);
+			Assert.AreEqual(41, res.Period.IncludedDays);
+		}
+
+		[Test()]
+		public void Calling_BusinessDurationService_WithFilter_ReturnsCorrectBusinessDuration()
+		{
+			// Arrange
+			var location = new LocationId("usa/anchorage");
+			var startDate = new DateTime(2017, 12, 1);
+			var endDate = new DateTime(2018, 1, 31);
+
+			// Act
+			var svc = new BusinessDurationService(Config.AccessKey, Config.SecretKey);
+			svc.Filter = BusinessDaysFilterType.Monday | BusinessDaysFilterType.Tuesday;
+
+			var res = svc.GetDuration(startDate, endDate, location);
+
+			// Assert
+			Assert.AreEqual("Anchorage", res.Geography.Name);
+
+			Assert.AreEqual(61, res.Period.CalendarDays);
+			Assert.AreEqual(18, res.Period.SkippedDays);
+			Assert.AreEqual(43, res.Period.IncludedDays);
+
+			Assert.AreEqual(9, res.Period.Weekdays.MondayCount);
+			Assert.AreEqual(9, res.Period.Weekdays.TuesdayCount);
+			Assert.AreEqual(0, res.Period.Weekdays.WednesdayCount);
+			Assert.AreEqual(0, res.Period.Weekdays.ThursdayCount);
+			Assert.AreEqual(0, res.Period.Weekdays.FridayCount);
+			Assert.AreEqual(0, res.Period.Weekdays.SaturdayCount);
+			Assert.AreEqual(0, res.Period.Weekdays.SundayCount);
+		}
+	}
 }
--- TimeAndDate.Services/BusinessDuration.cs
+++ TimeAndDate.Services/BusinessDuration.cs
@@ -1,10 +1,26 @@
 ﻿using System;
+using System.Collections.Generic;
+using TimeAndDate.Services.DataTypes.BusinessDays;
+using TimeAndDate.Services.DataTypes.Places;
+
 namespace TimeAndDate.Services
 {
-    public class BusinessDuration
-    {
-        public BusinessDuration()
-        {
-        }
-    }
+	public class BusinessDuration
+	{
+		/// <summary>
+		/// Geographical information about the location.
+		/// </summary>
+		/// <value>
+		/// The geography.
+		/// </value>
+		public Geo Geography { get; set; }
+
+		public Period Period { get; set; }
+
+		public BusinessDuration(Geo geo, Period period)
+		{
+			Geography = geo;
+            Period = period;
+		}
+	}
 }
