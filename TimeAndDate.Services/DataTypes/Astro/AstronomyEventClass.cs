d62824993acec510e6b99d52cdf841dcf0959eb0
Daniel Alvsåker <daniel@timeanddate.com>
2021-08-18 10:16:25 +0200

    Add 'Current' variant to AstronomyEventClass

--- TimeAndDate.Services/DataTypes/Astro/AstronomyEventClass.cs
+++ TimeAndDate.Services/DataTypes/Astro/AstronomyEventClass.cs
@@ -9,39 +9,43 @@ namespace TimeAndDate.Services.DataTypes.Astro
 		/// </summary>
 		All = 1,
 		/// <summary>
+		/// The current phase for the place requested. Additional attributes for illumination (moon), azimuth, distance. 
+		/// </summary>
+		Current = 1 << 1,
+		/// <summary>
 		/// Day length. 
 		/// </summary>
-		DayLength = 1 << 1,
+		DayLength = 1 << 2,
 		/// <summary>
 		/// Meridian (Noon, highest point) and Anti-Meridian (lowest point) events.
 		/// </summary>
-		Meridian = 1 << 2,
+		Meridian = 1 << 3,
 		/// <summary>
 		/// Moon phase events. Additionally to the phase events (only occurring on 
 		/// four days per lunar month), an additional attribute for the current moon 
 		/// phase is reported for every day.
 		/// </summary>
-		Phase = 1 << 3,
+		Phase = 1 << 4,
 		/// <summary>
 		/// Set and rise events. Event times take atmospheric refraction into account.
 		/// </summary>
-		SetRise = 1 << 4,
+		SetRise = 1 << 5,
 		/// <summary>
 		/// Combination of all 3 twilight classes.
 		/// </summary>
-		AllTwilights = 1 << 5,
+		AllTwilights = 1 << 6,
 		/// <summary>
 		/// Civil twilight (-6°).
 		/// </summary>
-		CivilTwilight = 1 << 6,
+		CivilTwilight = 1 << 7,
 		/// <summary>
 		/// Nautical twilight (-12°).
 		/// </summary>
-		NauticalTwilight = 1 << 7,
+		NauticalTwilight = 1 << 8,
 		/// <summary>
 		/// Astronomical twilight (-18°).
 		/// </summary>
-		AstronomicalTwilight = 1 << 8		
+		AstronomicalTwilight = 1 << 9
 	}
 }
 
